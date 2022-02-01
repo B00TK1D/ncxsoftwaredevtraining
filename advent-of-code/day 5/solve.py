bfb68e4b726656f047b7eb9e6d1f108485520bc2
ark-232 <arkady232@gmail.com>
2022-02-01 16:54:10 -0700

    day 5

---
+++ advent-of-code/day 5/solve.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    print("Advent of Code - Day 5")
+
+
+    a_file = open("input.txt", "r")
+
+    lines = a_file.read()
+    list_of_lists = lines.splitlines()
+
+    a_file.close()
+
+    print(list_of_lists)
+
+
+
+
+
+
+
+
